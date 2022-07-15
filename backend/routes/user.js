dee235b44cc91ee3781210f080de3268d0ee706d
Leorio21 <Ptitlue@gmail.com>
2022-07-15 21:27:09 +0200

    Feat: Securite
    Ajout d une limitation de creation de compte 5/1h

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit')
 const router = express.Router();
 
 const userCtrl = require('../controllers/user');
 const passwordValidator = require ('../middleware/password')
 
-router.post('/signup', passwordValidator, userCtrl.signup);
+const limiter = rateLimit({
+	windowMs: 60 * 60 * 1000, // 15 minutes
+	max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+router.post('/signup', limiter, passwordValidator, userCtrl.signup);
 
 router.post('/login', userCtrl.login);
 
